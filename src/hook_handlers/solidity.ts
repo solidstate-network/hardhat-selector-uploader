2012fa98afd43ead0bbf12076e7b23315866bdd0
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-15 02:04:32 -0600

    skip runOnCompile if running on CI server

--- src/hook_handlers/solidity.ts
+++ src/hook_handlers/solidity.ts
@@ -1,12 +1,13 @@
 import { uploadSelectors } from '../logic.js';
+import { isCi } from '@nomicfoundation/hardhat-utils/ci';
 import type { SolidityHooks } from 'hardhat/types/hooks';
 
 export default async (): Promise<Partial<SolidityHooks>> => ({
   onCleanUpArtifacts: async (context, artifactPaths, next) => {
-    // TODO: skip if running on CI server
     if (
       context.config.fourByteUploader.runOnCompile &&
-      !context.globalOptions.noUploadSelectors
+      !context.globalOptions.noUploadSelectors &&
+      !isCi()
     ) {
       await uploadSelectors(context);
     }
