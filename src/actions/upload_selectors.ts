6c5c28c2e303602d0f93d4e923ff2427dbef1da2
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-15 19:12:38 -0600

    fix task action import

--- src/actions/upload_selectors.ts
+++ src/actions/upload_selectors.ts
@@ -1,4 +1,4 @@
-import { uploadSelectors } from '../logic.js';
+import { uploadSelectors } from '../lib/selector_uploader.js';
 import type { NewTaskActionFunction } from 'hardhat/types/tasks';
 
 interface UploadSelectorsActionArguments {
